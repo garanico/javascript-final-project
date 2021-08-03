bf660b3f807aaeeb2fa02a2e0d50c6825b1f3465
Grace Aranico <grace.aranico@gmail.com>
2021-08-03 14:28:36 -0700

    replaced answers with images, deleted 8ball images

--- js/final-project.js
+++ js/final-project.js
@@ -1,18 +1,26 @@
+//define blank crystal ball image
+const startImage = document.getElementById("crystalBall");
+
+crystalBall.src = "./img/crystal-ball.png";
+
+//allows you to access the button 
 let button = document.querySelector("button");
+
+//allows you to access the text box
 let questionField = document.querySelector("#question-box");
+
+//allows you to access the div where answers are displayed
 let answerDiv = document.querySelector("#answer");
 
-let answer = ["Answer 1",
-"Answer 2",
-"Answer 3",
-"Answer 4",
-"Answer 5"];
+//create an image array for answers
+let answer = ['./img/magic8ball-1.png',
+'./img/magic8ball-2.png',
+'./img/magic8ball-3.png'];
 
 //add a click event to the button
 let displayAnswer = button.addEventListener("click", function(){
-    //Generate random number from the number of items in the array
-    answerDiv.innerText = (answer[Math.floor(Math.random() * answer.length)])
+    //Generate and display a random answer image from the array
+    document.getElementById("crystalBall").src = (answer[Math.floor(Math.random() * answer.length)])
     });
-    // console.log(displayAnswer);;
 
-// console.log(displayAnswer[Math.floor(Math.random() * answer.length)])
+
