dc0923c72c0332f19e67b1de37184d48d7ab3374
Grace Aranico <grace.aranico@gmail.com>
2021-08-04 22:02:07 -0700

    included if else statement to check if user inputted a question

--- js/final-project.js
+++ js/final-project.js
@@ -1,18 +1,16 @@
-//define blank crystal ball image
+//Define blank crystal ball image
 const startImage = document.getElementById("crystalBall");
 crystalBall.src = "./img/crystal-ball.png";
 
-//allows you to access the button 
+//Create a variable button allows you to access the button 
 let button = document.querySelector("button");
 
-//allows you to access the text box
-let questionField = document.querySelector("#question-box");
-
-//allows you to access the div where answers are displayed
+//Create variable answerDiv allows you to access the div where answers will be displayed
 let answerDiv = document.querySelector("#answer");
 
-//create an image array for answers
-let answer = ['./img/ball-1.png',
+//Create an image array for answers
+let answer = [
+'./img/ball-1.png',
 './img/ball-2.png',
 './img/ball-3.png',
 './img/ball-4.png',
@@ -20,12 +18,21 @@ let answer = ['./img/ball-1.png',
 './img/ball-6.png',
 './img/ball-7.png',
 './img/ball-8.png',
-'./img/ball-9.png'];
+'./img/ball-9.png'
+];
 
-//add a click event to the button
-let displayAnswer = button.addEventListener("click", function(){
-    //Generate and display a random answer image from the array
+
+//Confirms if content was entered into the text input field.
+let getAnswer= button.addEventListener("click", function(){
+    document.querySelector("questionBox"); 
+    getAnswer = input.value;
+     if(getAnswer == "")
+     alert("What\'s your inquiry?");
+     else{
+         //Generate and display a random answer image from the array
     document.getElementById("crystalBall").src = (answer[Math.floor(Math.random() * answer.length)]);
-    });
-
+    //clears text box after button click
+   document.getElementById("input").value = "";
+     }
+ });
 
